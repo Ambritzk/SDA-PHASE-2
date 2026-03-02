5445c1056f7c2dfff79460d43e80d9ac0b9d95ed
ABDUL0111 <90447172+ABDUL0111@users.noreply.github.com>
2026-03-02 23:06:24 +0500

    Add files via upload

--- plugins/input.py
+++ plugins/input.py
@@ -1,11 +1,9 @@
 import csv
-import json
-#import pandas as pd
 from typing import List, Dict, Any
 from pathlib import Path
 from core.contracts import PipelineService
 
-class CSVReader:
+class CsvReader:
     def __init__(self, service: PipelineService) -> None:
         self.service = service
         file_path = Path(__file__).parent.parent.absolute() / 'data' / 'gdp_with_continent_filled.xlsx - GDP.csv'
@@ -15,22 +13,3 @@ class CSVReader:
 
     def run(self) -> None:
         self.service.execute(self.data)
-
-
-class JSONReader:
-    service: PipelineService
-    data: List[dict] 
-    def __init__(self, ServiceFromCore: PipelineService) -> None:
-        self.service = ServiceFromCore
-        #First we need to get the csv file. We therefore go to the parent directory and then enter into the data directory
-        file_path = Path(__file__).parent.parent.absolute() / 'data' / 'gdp_with_continent_filled.json'
-        with open(file_path, "r", encoding="utf-8") as file:
-            raw_data = file.read()
-            valid = raw_data.replace('#@$!\\','NaN').replace('NaN','0')
-            final = json.loads(valid)
-            self.data = final
-
-            
-    def run(self):
-        self.service.execute(self.data)
-
